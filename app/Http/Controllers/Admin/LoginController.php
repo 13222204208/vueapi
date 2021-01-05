5f5861e56c915392fd10ecf88cd9e684ea172613
zjp <360943749@qq.com>
2021-01-05 19:17:13 +0800

    2021.1.5

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\UploadImageController;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $upload = new UploadImageController;
+        return $upload->getNewFile($request->file('img'));
+    }
 }
